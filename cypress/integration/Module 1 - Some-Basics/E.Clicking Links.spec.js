198312d6057d49a71b60b8118d68115e73294928
abellifemine <abellifemine@bluegracegroup.com>
2020-12-18 20:13:46 -0500

    done

--- cypress/integration/Module 1 - Some-Basics/E.Clicking Links.spec.js
+++
@@ -1,8 +0,0 @@
-
-describe('Click Sign In link', () => {
-    it('Make sure log in page displays', () => {
-        cy.viewport(1920, 1080);
-        cy.visit('');
-        cy.get('a').contains("Sign in").click()
-    });
-});
